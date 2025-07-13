db45506b24bd4e97b2a95c71463c51cbc4a6fe6d
Asmae Misbah <asmaemisbah091@gmail.com>
2025-07-13 21:40:14 +0200

    update

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
         <title>AI Image Colorization</title>
         <meta name="description" content="AI-powered image colorization using U-Net model" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
       
       <Component {...pageProps} />
